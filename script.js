4f73f60809944b05a945d2ce4b23108c511cb48e
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-28 08:47:14 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let arr=["applause.mp3","boo.mp3","gasp.mp3","tada.mp3","victory.mp3","wrong.mp3"];
+
+let btns=document.getElementsByTagName('buttons');
+let audio;
+for(let i=0;i<btns.length-1;i++){
+    btns[i].addEventListener('click',()=>{
+    audio=new Audio(`sounds/${audioArr[i]}`);
+    audio.play();
+    })
+}
+
+btns[btns.length-1].addEventListener('click',()=>{
+audio.pause();
+})
